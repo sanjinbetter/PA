b4004f434637d85b5b0559ea0c82749c62e2e57f
lixin <lixin@547060493@qq.com>
2024-05-29 17:37:00 +0800

    del param

--- chains/assistant_chain.py
+++ chains/assistant_chain.py
@@ -8,7 +8,7 @@ from util import LOG
 基础聊天链
 """
 class AssistantChain:
-    def __init__(self, llm, assistant_message: str = "你是一个通用的生活助理，能回答所有生活中的问题", verbose: bool = True):
+    def __init__(self, llm, assistant_message: str = "你是一个通用的生活助理，能回答所有生活中的问题"):
         system_prompt = SystemMessagePromptTemplate.from_template(assistant_message)
         human_prompt = HumanMessagePromptTemplate.from_template("请回答问题: {question}")
 
